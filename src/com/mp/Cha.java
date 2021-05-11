7fcecc865821af24c786a147d029e4d168693b98
Sophia Oquias <sophia_oquias@dlsu.edu.ph>
2021-05-11 12:35:43 +0800

    Created Ord Class

--- src/com/mp/Cha.java
+++ src/com/mp/Cha.java
@@ -1,5 +1,5 @@
 package com.mp;
 
 public class Cha extends Space {
-    
+
 }
---
+++ src/com/mp/Ord.java
@@ -0,0 +1,17 @@
+package com.mp;
+
+public class Ord extends Space {
+
+    // Methods
+    public void setPos(int count, int row, int col, Board board) {
+        if(count < 4) {
+            count++;
+            board.setSpace(row, col, this);
+        }
+        else if(count == 4) {
+            count -= 2;
+            board.setSpace(row, col, new Free());
+            
+        }
+    }
+}
