e039c37b36a34786e5bf3347710796d912dc3639
Sophia Oquias <sophia_oquias@dlsu.edu.ph>
2021-05-16 11:21:59 +0800

    Added variables in Driver class

---
+++ src/com/mp/Driver.java
@@ -0,0 +1,10 @@
+package com.mp;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        boolean over = false;
+        boolean turn = true;
+        int ordCount = 0;
+    }
+}
