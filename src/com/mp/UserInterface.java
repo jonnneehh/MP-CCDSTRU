a971cf50b2a3cef66dbee30698742483af999de4
jonnneehh <jon_llamado@dlsu.edu.ph>
2021-05-20 23:23:44 +0800

    UserInterface documentation

--- src/com/mp/UserInterface.java
+++ src/com/mp/UserInterface.java
@@ -2,10 +2,12 @@ package com.mp;
 
 public class UserInterface {
 	
+	//Displays instructions of the game
 	public void displayGameInstructions() {
 		System.out.println("Instructions:");
 	}
 	
+	//Enter key must be pressed for user to continue
 	public void pressAnyKeyToContinue(){ 
 	        System.out.println("Press Enter key to continue...");
 	        try
@@ -15,7 +17,8 @@ public class UserInterface {
 	        catch(Exception e)
 	        {}  
 	 }
-	 
+	
+	//Displays 2d array board by checking what is the instance from getSpace
 	public void displayBoard(Board board) {
 		for(int row = 0; row < 4; row++)
 			for(int col = 0; col < 4; col++) {
@@ -31,6 +34,7 @@ public class UserInterface {
 				
 	}
 	
+	//Shows the winner using checkChaWin from system manager
 	public void showWinner(boolean isChaWinner) {
 		if (isChaWinner == true) {
 			System.out.println("Cha is the winner!");
