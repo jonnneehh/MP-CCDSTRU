748759e74bf89454e82f8b8bbe244c6c5c728fcd
Sophia Oquias <sophia_oquias@dlsu.edu.ph>
2021-05-21 21:17:59 +0800

    fixed bug with Ord turn

--- src/com/mp/Driver.java
+++ src/com/mp/Driver.java
@@ -12,35 +12,25 @@ public class Driver {
     /* Displays message asking for row number and
      * returns the inputted number    */
     public static int getRowInput() {
-<<<<<<< HEAD
-        System.out.print("Input row: ");
-        return Integer.parseInt(sc.nextLine()) - 1;
-=======
         int row;
         do{
-        	System.out.print("Input row: ");
-    		row = Integer.parseInt(sc.nextLine());
-    	}while(row <= 0 || row >= 4);
-    	
-        return row;
->>>>>>> 1e0baecfb13304f9a6c28a4b21520f16e3ad368f
+            System.out.print("Input row: ");
+            row = Integer.parseInt(sc.nextLine());
+        }while(row < 1 || row > 4);
+
+        return row - 1;
     }
 
     /* Displays message asking for column number and
      * returns the inputted number    */
     public static int getColInput() {
-<<<<<<< HEAD
-        System.out.print("Input column: ");
-        return Integer.parseInt(sc.nextLine()) - 1;
-=======
         int col;
         do {
-        	System.out.print("Input column: ");
-    		col = Integer.parseInt(sc.nextLine());
-    	}while(col <= 0 || col >= 4);
-        
-        return col;
->>>>>>> 1e0baecfb13304f9a6c28a4b21520f16e3ad368f
+            System.out.print("Input column: ");
+            col = Integer.parseInt(sc.nextLine());
+        }while(col < 1 || col > 4);
+
+        return col - 1;
     }
 
     public static void main(String[] args) {
@@ -52,20 +42,12 @@ public class Driver {
         Board board = new Board();
         UserInterface userInterface = new UserInterface();
 
-<<<<<<< HEAD
-        userInterface.displayGameInstructions(); /***No instructions written yet***/
-        userInterface.pressAnyKeyToContinue();
-
-        while(over == false) {
-
-=======
 
         userInterface.displayGameInstructions(); /***No instructions written yet***/
 
         while(!over) {
 
             //userInterface.pressAnyKeyToContinue();
->>>>>>> 1e0baecfb13304f9a6c28a4b21520f16e3ad368f
             userInterface.displayBoard(board);
 
             if(turn) {
@@ -98,24 +80,12 @@ public class Driver {
                 int row = getRowInput();
                 int col = getColInput();
 
-<<<<<<< HEAD
                 // executes if there are less than 4 Ord pieces on the board
                 if(systemManager.countOrd(board) < 3) {
-
-                    // Check if chosen space is a Free space
-                    if(board.getSpace(row, col) instanceof Free) {
-
-=======
-                ordCount++;
-
-                // executes if there are less than 4 Ord pieces on the board
-                if(ordCount < 4) {
                     // Check if chosen space is a Free space
                     if(board.getSpace(row, col) instanceof Free) {
                         turn = false;
->>>>>>> 1e0baecfb13304f9a6c28a4b21520f16e3ad368f
                         board.setSpace(row, col, new Ord());
-                        turn = !turn;
 
                         // method to display board here
                     }
@@ -140,7 +110,7 @@ public class Driver {
                 over = systemManager.checkOrdWin(board);
             }
         }
-        
+
         userInterface.showWinner(systemManager.checkChaWin(board));
     }
 }
